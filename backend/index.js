5fff9e182da5bc6558756d1ea5c299bcf78773a4
alikhere <alikhurshidhere@gmail.com>
2025-02-12 22:38:15 +0530

    backend finished

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,46 @@
 import express from "express"
 import mongoose from "mongoose"
-import dotevn from "dotenv"
+import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
+dotenv.config()
 
-dotevn.config()
-mongoose.connect(process.env.MONGO_URI)
-.then(()=> {
-    console.log("Connted to mangoDB")
-})
-.catch((err)=> {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongoDB")
+  })
+  .catch((err) => {
     console.log(err)
-})
+  })
 
 const app = express()
 
-//to make input as json
+// to make input as json
 app.use(express.json())
-app.use(cookieParser)
-app.use(cors({origin: "*"}))
+app.use(cookieParser())
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
 
-app.listen(3000, ()=> {
-    console.log("Server is ruuning on PORT 3000")
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
 })
 
-//import routes
+// import routes
 import authRouter from "./routes/auth.route.js"
+import noteRouter from "./routes/note.route.js"
 
 app.use("/api/auth", authRouter)
+app.use("/api/note", noteRouter)
 
-
-
-//error handling
+// error handling
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || "Internal Server Error"
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    })
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Serer Error"
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
 })
