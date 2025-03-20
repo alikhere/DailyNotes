ae8a9bcee4b46bbbd0e7751cf456e5e5b25c8e8d
alikhere <alikhurshidhere@gmail.com>
2025-03-21 05:02:43 +0530

    add signout route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -58,3 +58,16 @@ export const signin = async (req, res, next) => {
         
     }
 }
+
+export const signout = async (req, res, next) => {
+    try {
+      res.clearCookie("access_token")
+  
+      res.status(200).json({
+        success: true,
+        message: "User logged out successfully",
+      })
+    } catch (error) {
+      next(error)
+    }
+}
