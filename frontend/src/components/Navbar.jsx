39eb3e4fa2aa6f3c40d2a232e7c190487d113822
alikhere <alikhurshidhere@gmail.com>
2025-03-23 00:58:01 +0530

    integrating navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,36 +1,73 @@
-import React, { useState } from 'react'
-import SearchBar from './SearchBar/SearchBar'
-import ProfileInfo from './Cards/ProfileInfo'
-import { useNavigate, Link } from 'react-router-dom'
+import React, { useState } from "react"
+import SearchBar from "./SearchBar/SearchBar"
+import ProfileInfo from "./Cards/ProfileInfo"
+import { Link, useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { toast } from "react-toastify"
+import {
+  signInSuccess,
+  signoutFailure,
+  signoutStart,
+} from "../redux/user/userSlice"
+import axios from "axios"
 
-function Navbar() {
-    const [searchQuery, setSearchQuery] = useState("")
-    const navigate = useNavigate()
+const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("")
 
-    const handleSearch = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const handleSearch = () => {
+    if (searchQuery) {
+      onSearchNote(searchQuery)
     }
+  }
 
-    const onClearSearch = () => {
-      setSearchQuery("")
-    }
+  const onClearSearch = () => {
+    setSearchQuery("")
+    handleClearSearch()
+  }
 
-    const onLogout = () => {
-        navigate("/Login")
+  const onLogout = async () => {
+    try {
+      dispatch(signoutStart())
+
+      const res = await axios.get("http://localhost:3000/api/auth/signout", {
+        withCredentials: true,
+      })
+
+      if (res.data.success === false) {
+        dispatch(signoutFailure(res.data.message))
+        toast.error(res.data.message)
+        return
+      }
+
+      toast.success(res.data.message)
+      dispatch(signInSuccess())
+      navigate("/login")
+    } catch (error) {
+      toast.error(error.message)
+      dispatch(signoutFailure(error.message))
     }
+  }
+
   return (
-    <div className='bg-white flex justify-between items-center px-6 py-2 drop-shadow'>
-        <Link to= {"/"}> 
-          <h2 className='text-xl font-medium text-black py-2'>
-              <span
-              className='text-slate-500'>Daily
-              </span>
-              <span
-              className='text-slate-900'>Notes
-              </span>
-          </h2>
-        </Link>
-        <SearchBar value = {searchQuery} onChange = {(e) => setSearchQuery(e.target.value)}/>
-        <ProfileInfo onLogout={onLogout}/>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <Link to={"/"}>
+        <h2 className="text-xl font-medium text-black py-2">
+          <span className="text-slate-500">Good</span>
+          <span className="text-slate-900">Notes</span>
+        </h2>
+      </Link>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => setSearchQuery(target.value)}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
+      <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
     </div>
   )
 }
