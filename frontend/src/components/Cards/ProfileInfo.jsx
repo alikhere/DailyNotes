2a7f897001672e7f06208d42f93dc2109b8f37d9
alikhere <alikhurshidhere@gmail.com>
2025-03-23 01:06:36 +0530

    upaded profile info

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { getInitials } from '../../utils/helper'
+import React from "react"
+import { getInitials } from "../../utils/helper"
 
-const ProfileInfo = ({onLogout}) => {
+const ProfileInfo = ({ onLogout, userInfo }) => {
   return (
-    <div className='flex items-center gap-3'>
-        <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-m bg-slate-100'>
-            {getInitials("Rohan Singh")}
-        </div>
-        <div>
-            <p className='text-sm font-medium'>Rohan </p>
-        </div>
-        <button className='text-sm bg-red-500 p-1 rounded-md text-white hover:opacity-80' onClick={onLogout}>
-            Logout
-        </button>
+    <div className="flex items-center gap-3">
+      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+        {getInitials(userInfo?.username)}
+      </div>
+
+      <div>
+        <p className="text-sm font-medium">{userInfo?.username}</p>
+      </div>
+
+      <button
+        className="text-sm bg-red-500 p-1 rounded-md text-white hover:opacity-80"
+        onClick={onLogout}
+      >
+        Logout
+      </button>
     </div>
   )
 }
