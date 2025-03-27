41c1a6fceb01c0126d3bc23ccf71db794cc74b91
alikhere <alikhurshidhere@gmail.com>
2025-03-27 23:57:46 +0530

    integrated addEditNotes with backend

--- frontend/src/pages/Home/AddEditNotes.jsx
+++ frontend/src/pages/Home/AddEditNotes.jsx
@@ -1,67 +1,138 @@
-import React, { useState } from 'react'
-import { MdClose } from 'react-icons/md'
-import TagInput from '../../components/Input/TagInput'
+import React, { useState } from "react"
+import { MdClose } from "react-icons/md"
+import TagInput from "../../components/Input/TagInput "
+import axios from "axios"
+import { toast } from "react-toastify"
 
-function AddEditNotes({onClose, noteData, type}) {
-    const [title, setTitle] = useState("")
-    const [content, setConent] = useState("")
-    const [tags, setTags] = useState([])
-    const [error, setError] = useState(null)
+const AddEditNotes = ({ onClose, noteData, type, getAllNotes }) => {
+  const [title, setTitle] = useState(noteData?.title || "")
+  const [content, setContent] = useState(noteData?.content || "")
+  const [tags, setTags] = useState(noteData?.tags || [])
+  const [error, setError] = useState(null)
 
-    const editNote = async () => {
+  //   Edit Note
+  const editNote = async () => {
+    const noteId = noteData._id
+    console.log(noteId)
 
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/api/note/edit/" + noteId,
+        { title, content, tags },
+        { withCredentials: true }
+      )
+
+      console.log(res.data)
+
+      if (res.data.success === false) {
+        console.log(res.data.message)
+        setError(res.data.message)
+        toast.error(res.data.message)
+        return
+      }
+
+      toast.success(res.data.message)
+      getAllNotes()
+      onClose()
+    } catch (error) {
+      toast.error(error.message)
+      console.log(error.message)
+      setError(error.message)
+    }
+  }
+
+  //   Add Note
+  const addNewNote = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/api/note/add",
+        { title, content, tags },
+        { withCredentials: true }
+      )
+
+      if (res.data.success === false) {
+        console.log(res.data.message)
+        setError(res.data.message)
+        toast.error(res.data.message)
+
+        return
+      }
+
+      toast.success(res.data.message)
+      getAllNotes()
+      onClose()
+    } catch (error) {
+      toast.error(error.message)
+      console.log(error.message)
+      setError(error.message)
+    }
+  }
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter the title")
+      return
     }
 
-    const addNewNote = async () => {
-
+    if (!content) {
+      setError("Please enter the content")
+      return
     }
-    const handleAddNote = ()=> {
-        if(!title) {
-            setError("Please enter the title")
-            return
-        }
-        if(!content) {
-            setError("Please enter the content")
-        }
-        setError("")
-        if(type === 'edit')
-            editNote()
-        else
-            addNewNote()
 
+    setError("")
+
+    if (type === "edit") {
+      editNote()
+    } else {
+      addNewNote()
     }
+  }
 
   return (
-    <div className='relative ' >
-        <button className='w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50' onClick={onClose}
-        >
-        < MdClose className='text-xl text-slate-400 '/>
-        </button>
-        <div  className='flex flex-col gap-2'>
-            <label className='input-label text-red-400 uppercase '> Title</label>
-            <input type="text" className=' text-2xl text-slate-950 outline-none '
-            placeholder='Enter title'
-            value={title}
-            onChange={(e)=> setTitle(e.target.value)}
-            />
-        </div>
-        <div className='flex flex-col gap-2 mt-4'>
-            <label className='input-label text-red-400 uppercase '>Content</label>
+    <div className="relative">
+      <button
+        className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50"
+        onClick={onClose}
+      >
+        <MdClose className="text-xl text-slate-400" />
+      </button>
+      <div className="flex flex-col gap-2">
+        <label className="input-label text-red-400 uppercase">Title</label>
 
-            <textarea type="text" className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded '
-            placeholder='Content...'
-            value={content}
-            onChange={({target})=> setConent(target.value)}>
-            </textarea>
-        </div>
-        <div className='flex flex-col gap-2 mt-4'>
-            <label className='input-label text-red-400 uppercase '>tags</label>
-            <TagInput tags={tags} setTags={setTags}/>
-        </div>
+        <input
+          type="text"
+          className="text-2xl text-slate-950 outline-none"
+          placeholder="Wake up at 6 a.m."
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div className="flex flex-col gap-2 mt-4">
+        <label className="input-label text-red-400 uppercase">Content</label>
 
-        {error && <p className='text-red-500 text-xs pt-4'> {error}</p> }
-        
-        <button className='btn-primary font-medium at-5 p-3 ' onClick={handleAddNote}>ADD</button>
+        <textarea
+          type="text"
+          className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+          placeholder="Content..."
+          rows={10}
+          value={content}
+          onChange={({ target }) => setContent(target.value)}
+        />
+      </div>
+
+      <div className="mt-3">
+        <label className="input-label text-red-400 uppercase">tags</label>
+        <TagInput tags={tags} setTags={setTags} />
+      </div>
+
+      {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+      <button
+        className="btn-primary font-medium mt-5 p-3"
+        onClick={handleAddNote}
+      >
+        {type === "edit" ? "UPDATE" : "ADD"}
+      </button>
     </div>
   )
 }
