912306322de0df930bd9e0f627d255e1e363c2d4
alikhere <alikhurshidhere@gmail.com>
2025-03-28 05:07:49 +0530

    add search note feature

--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -9,6 +9,6 @@ router.post("/edit/:noteId", verifyToken, editNote)
 router.get("/all", verifyToken, getAllNotes)
 router.delete("/delete/:noteId", verifyToken, deleteNote)
 router.put("/update-note-pinned/:noteId", verifyToken, updateNotePinned)
-
+router.get("/search", verifyToken, searchNote)
 
 export default router
