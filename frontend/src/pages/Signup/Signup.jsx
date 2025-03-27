1b3e4a5c3e5e1df8b9f6a8277f9c47f1c9442b6d
alikhere <alikhurshidhere@gmail.com>
2025-03-27 23:47:57 +0530

    add signup api

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,61 +1,111 @@
-import React, { useState } from 'react'
-import PasswordInput from '../../components/Input/PasswordInput'
-import {Link} from "react-router-dom"
-import { validateEmail } from '../../utils/helper'
+import React, { useState } from "react"
+import PasswordInput from "../../components/Input/PasswordInput"
+import { Link, useNavigate } from "react-router-dom"
+import { validateEmail } from "../../utils/helper"
+import axios from "axios"
+import { toast } from "react-toastify"
 
-
-
-function Signup() {
+const Signup = () => {
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
-  const [name, setName] = useState("")
 
-  const handleSignup = async(e) => {
+  const navigate = useNavigate()
+
+  const handleSignUp = async (e) => {
     e.preventDefault()
 
-    if(!name) {
+    if (!name) {
       setError("Please enter your name")
       return
     }
-    if(!validateEmail(email)) {
+
+    if (!validateEmail(email)) {
       setError("Please enter a valid email address")
       return
     }
-    if(!password) {
+
+    if (!password) {
       setError("Please enter the password")
       return
     }
-    setError("")
-    
-    //signupAPI
 
+    setError("")
+
+    // sign up api
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/api/auth/signup",
+        { username: name, email, password },
+        { withCredentials: true }
+      )
+
+      if (res.data.success === false) {
+        setError(res.data.message)
+        toast.error(res.data.message)
+        return
+      }
+
+      toast.success(res.data.message)
+
+      setError("")
+
+      navigate("/login")
+    } catch (error) {
+      toast.error(error.message)
+      console.log(error.message)
+      setError(error.message)
+    }
   }
 
   return (
-    <div className='flex items-center justify-center mt-28'>
-        <div className='w-96 border rounded-md bg-white px-7 py-10'>
-            <form onSubmit={handleSignup} action="">
+    <>
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">Sign Up</h4>
 
-              <h4 className='text-2xl mb-7'>Sign Up</h4>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
 
-              <input type="text" placeholder='Name' className='input-box' value={name} onChange={(e) => setName(e.target.value)} />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-              <input type="text" placeholder='Email' className='input-box' value={email} onChange={(e) => setEmail(e.target.value)} />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-              <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} 
-              />
+            {error && <p className="text-red-500 text-sm pb-1">{error}</p>}
 
-              {error && <p className='text-red-500 text-sm pb-1'>{error}</p>}
+            <button type="submit" className="btn-primary">
+              SIGN UP
+            </button>
 
-            <button type='submit' className='btn-primary'>Sign up</button>
-
-            <p className='text-sm text-center mt-4'> Already have an account?{" "}
-              <Link to={"/login"} className='font-medium text-[#2B85FF]'>Log in</Link> 
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link
+                to={"/login"}
+                className="font-medium text-[#2B85FF] underline"
+              >
+                Login
+              </Link>
             </p>
-            </form>
+          </form>
         </div>
-    </div>
+      </div>
+    </>
   )
 }
 
